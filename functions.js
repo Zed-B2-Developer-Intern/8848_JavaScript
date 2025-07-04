d19d77878501bf6e0f853753973cee497f5bc4f5
RoshiniNarayanan <narayananroshini9@gmail.com>
2025-07-04 19:46:11 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+let length,breadth;
+length=5;
+breadth=4;
+function caculateperimeter(l,b)
+{
+    return l*b;
+}
+result=caculateperimeter(length,breadth);
+console.log("Perimeter of rectangle with "+length+ "cm as L and "+breadth+"cm as B is "+result+"cm");
