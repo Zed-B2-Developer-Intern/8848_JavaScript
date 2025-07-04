eff81b21c29db76b0ef6d9b244ab0c62c53e2152
RoshiniNarayanan <narayananroshini9@gmail.com>
2025-07-04 19:47:34 +0530

    Conditionals

---
+++ conditionals.js
@@ -0,0 +1,49 @@
+let pin=1234;
+let orgpin=1234;
+let bal=70000;
+if(pin==orgpin)
+{
+    console.log("Valid pin");
+    console.log("Available Balance:"+bal);
+    console.log(`
+    1.Withdraw
+    2.Deposit
+    3.Exit`)
+
+let option=2;
+switch(option)
+{
+    case 1:
+        let wdamt=6000;
+        if(wdamt<=bal && wdamt<10001)
+        {
+            bal=bal-wdamt;
+            console.log("Balance after withdrawl:"+bal);
+            break;
+        }
+        else if (wdamt>=bal)
+        {
+            console.log("Insufficient Balance");
+            break;
+        }
+        else
+        {
+            console.log("Withdrawl limit is 10,000 Rs only");
+            break;
+        }
+    case 2:
+        let depamt=1000;
+        bal+=depamt;
+        console.log("Balance after Deposition:"+bal);
+        break;
+    case 3:
+        return;
+        break;
+    default:
+        console.log("Choose the above listed options only");
+        break;
+}}
+else
+{
+    console.log("Invalid pin");
+}
